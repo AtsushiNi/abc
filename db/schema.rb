41d3288ab42f57e06a68ab49eb771f43ed8fc667
Atsushi <atsushi9731@gmail.com>
2018-12-25 18:49:39 +0900

    Finish chapter7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_25_065249) do
+ActiveRecord::Schema.define(version: 2018_12_25_044913) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
