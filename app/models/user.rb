ac89ad897e75a44b4e690f1d5f1624c6e86678e2
Atsushi <atsushi9731@gmail.com>
2018-12-25 19:01:16 +0900

    Re:chapter6

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REDEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255}, format: { with: VALID_EMAIL_REDEX }, uniqueness: { case_sensetive: false }
+  validates :email, presence: true, length: { maximum: 255}, format: { with: VALID_EMAIL_REDEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 end
